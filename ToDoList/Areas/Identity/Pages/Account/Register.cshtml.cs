7c1a20193c1ee92dbbb5cc16e0fd4d9db571dcc7
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-05-03 18:21:50 +0300

    Identity release 1.01 Added functionality that when registering a new user, he is assigned a user role.

--- ToDoList/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ToDoList/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -91,6 +91,7 @@ namespace ToDoList.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    await _userManager.AddToRoleAsync(user, "user");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
