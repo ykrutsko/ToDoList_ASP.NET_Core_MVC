7109cb78ba772f291a7610d5970e5e9b36025e6a
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-05-03 21:00:53 +0300

    SettingsController added Index action

---
+++ ToDoList/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class Settings : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public Settings(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
