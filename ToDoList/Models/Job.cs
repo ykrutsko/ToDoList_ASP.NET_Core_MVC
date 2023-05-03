6407bf2269172212b200c23f053b9ba1d7e43a5c
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-05-03 13:34:26 +0300

    Job model released

---
+++ ToDoList/Models/Job.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
+
+namespace ToDoList.Models
+{
+    public class Job
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Creation date")]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        [Display(Name = "Deadline")]
+        public DateTime DueDate { get; set; }
+
+        public string CreationUser { get; set; }
+
+        public bool IsDone { get; set; }
+
+        [Display(Name = "Priority")]
+        public int JobPriorityId { get; set; }
+
+        public JobPriority JobPriority { get; set; }
+
+        [Display(Name = "Satatus")]
+        public int JobStatusId { get; set; }
+
+        public JobStatus JobStatus { get; set; }
+    }
+}
