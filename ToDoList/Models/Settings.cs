920f780415b0dd8959119781b64dc2bfe48aa3c4
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-05-03 13:41:14 +0300

    Settings model released

---
+++ ToDoList/Models/Settings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Settings
+    {
+        public int PriorityId { get; set; }
+
+        [Required]
+        public string PriorityName { get; set; }
+
+        public int StatusId { get; set; }
+        [Required]
+        public string StatusName { get; set; }
+
+        public List<JobPriority> JobPriorities { get; set; }
+        public List<JobStatus> JobStatuses { get; set; }
+    }
+}
