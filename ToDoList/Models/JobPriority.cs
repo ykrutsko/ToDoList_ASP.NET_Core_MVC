50106e0f339cc0ddf33b9dec91e15370231ee995
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-05-03 13:35:42 +0300

    JobPriority, JobStatus models released

---
+++ ToDoList/Models/JobPriority.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.Models
+{
+    public class JobPriority
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ToDoList/Models/JobStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.Models
+{
+    public class JobStatus
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
