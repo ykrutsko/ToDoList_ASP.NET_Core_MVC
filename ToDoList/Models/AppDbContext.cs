68591c94ab88e060f9ce6b6b53f37d064f7acf51
Yurii Krutsko <yuri.krutsko@gmail.com>
2023-05-03 14:49:50 +0300

    AppDbContext model added

---
+++ ToDoList/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class AppDbContext : DbContext
+    {        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Job> Jobs { get; set; }
+        public DbSet<JobPriority> JobPriorities { get; set; }
+        public DbSet<JobStatus> JobStatuses { get; set; }
+    }
+}
